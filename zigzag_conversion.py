9353c1cb2fb9948fd3e20f093e6af925fdd22513
JoseDiegoMarinContreras <DIEGOmai195@hotmail.com>
2022-09-08 18:03:50 -0600

    Refactoring zigzag convert method

--- zigzag_conversion.py
+++ zigzag_conversion.py
@@ -1,31 +1,13 @@
-from functools import reduce
 def convert(s: str, numRows: int) -> str:
-    if numRows == 1:
+    if numRows == 1 or numRows >= len(s):
         return s
+    i, step = 0, 1
+    zigzag_map = numRows*[""]
+    for char in s:
+        zigzag_map[i] += char
+        step = 1 if i == 0 else -1 if i == numRows-1 else step
+        i += step
 
-    s_index = 0
-    zigzag_map = [[] for r in range(numRows)]
-    col_num = 0
-    zigzag_interval = numRows - 2
-
-    while s_index < len(s):
-        for i in range(numRows):
-            if col_num == 0 and s_index < len(s):
-                zigzag_map[i].append(s[s_index])
-                s_index += 1
-            elif i == zigzag_interval and s_index < len(s):
-                zigzag_map[i].append(s[s_index])
-                s_index += 1
-                zigzag_interval -= 1
-
-            
-
-        col_num += 1
-        if col_num == numRows - 1:
-            zigzag_interval = numRows - 2
-            col_num = 0
-
-    zigzag_map = reduce(lambda a,b: a+b, zigzag_map)
     return "".join(zigzag_map)
         
 # PAYPALISHIRING
@@ -43,9 +25,3 @@ print(convert(val, 4) == "PINALSIGYAHRPI")
 
 # PHASIYIRPLIGAN
 print(convert(val, 5) == "PHASIYIRPLIGAN")
-
-a = [
-["P", "", "", "I", "",  "", "N",],
-["A", "", "L", "S", "", "I", "G",],
-["Y", "A", "", "H", "R", "", "",],
-["P", "", "", "I", "", "",  "",],]
