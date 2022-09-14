b35660b388f6502cacc3ab9a6657cf357a32c385
JoseDiegoMarinContreras <DIEGOmai195@hotmail.com>
2022-09-13 18:29:36 -0600

    Refactoring roman_to_integer method to improve performance and memory usage

--- roman_to_integer.py
+++ roman_to_integer.py
@@ -3,18 +3,28 @@ from tkinter.font import ROMAN
 
 def int_to_roman(num: int) -> str:
     ROMAN_NUMS = {
-        0: { 0: "", 1: "I", 2: "II", 3: "III", 4: "IV", 5: "V", 6: "VI", 7: "VII", 8: "VIII", 9: "IX", },
-        1: { 0: "", 1: "X", 2: "XX", 3: "XXX", 4: "XL", 5: "L", 6: "LX", 7: "LXX", 8: "LXXX", 9: "XC", },
-        2: { 0: "", 1: "C", 2: "CC", 3: "CCC", 4: "CD", 5: "D", 6: "DC", 7: "DCC", 8: "DCCC", 9: "CM", },
-        3: { 0: "", 1: "M", 2: "MM", 3: "MMM" },
+        1000: "M",
+        900: "CM",
+        500: "D",
+        400: "CD",
+        100: "C",
+        90: "XC",
+        50: "L",
+        40: "XL",
+        10: "X",
+        9: "IX",
+        5: "V",
+        4: "IV",
+        1: "I",
     }
+    roman = ""
 
-    return "%s%s%s%s"%(
-                        ROMAN_NUMS[3][num//1000],
-                        ROMAN_NUMS[2][(num%1000)//100],
-                        ROMAN_NUMS[1][((num%1000)%100)//10],
-                        ROMAN_NUMS[0][((num%100)%100)%10],
-                      )
+    for s in ROMAN_NUMS:
+        roman += ROMAN_NUMS[s] * (num // s)
+        num %= s
+
+    return roman
+
 #III
 num = 3
 print(int_to_roman(num))
